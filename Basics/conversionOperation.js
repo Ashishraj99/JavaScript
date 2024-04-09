711948397ef37b771cc69cd70f8dd3681658958c
Ashish raj <81153070+Ashishraj99@users.noreply.github.com>
2024-04-09 06:22:37 +0000

    Conversion of datatypes

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,13 @@
+let score = 33
+let mark = "33"
+console.log(typeof score);
+console.log(typeof mark);
+
+let valuseInNumber = Number(score)
+console.log(typeof valuseInNumber);
+console.log(valuseInNumber);
+
+// "33" = 33
+// 33adb = NaN
+// true = 1
+// false = 0
